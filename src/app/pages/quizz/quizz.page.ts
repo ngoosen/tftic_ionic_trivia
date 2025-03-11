76616d744d0e2f7200306fa04bf8cb6ee199e863
Noémie Goosen <noemie.goosen@gmail.com>
2025-03-11 16:50:11 +0100

    feat: get params from url in quizz component

--- src/app/pages/quizz/quizz.page.ts
+++ src/app/pages/quizz/quizz.page.ts
@@ -1,7 +1,9 @@
-import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { ActivatedRoute } from '@angular/router';
 import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/standalone';
+import { ApiService, ITriviaQuestion } from 'src/app/services/api.service';
 
 @Component({
   selector: 'app-quizz',
@@ -11,10 +13,20 @@ import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/stan
   imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
 })
 export class QuizzPage implements OnInit {
+  questions: ITriviaQuestion[] = [];
 
-  constructor() { }
+  constructor(private _activatedRoute: ActivatedRoute, private _api: ApiService) { }
 
   ngOnInit() {
+    const params = this._activatedRoute.snapshot.queryParams;
+    this._api.getQuestions(params["amount"], params["category"], params["difficulty"]).subscribe({
+      next: (data) => {
+        console.log("🚀 ~ QuizzPage ~ this._api.getQuestions ~ data:", data);
+      },
+      error: (e) => {
+        console.log(e);
+      },
+    });
   }
 
 }
