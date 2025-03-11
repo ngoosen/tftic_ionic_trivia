9e656997f11363336142d167c48b1f80d0282217
Noémie Goosen <noemie.goosen@gmail.com>
2025-03-11 15:56:40 +0100

    feat: implement home page

--- src/app/pages/home/home.page.ts
+++ src/app/pages/home/home.page.ts
@@ -1,20 +1,60 @@
-import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/standalone';
+import { IonButton, IonContent, IonHeader, IonItem, IonLabel, IonList, IonRange, IonSelect, IonSelectOption, IonTitle, IonToolbar } from '@ionic/angular/standalone';
+import { ApiService } from 'src/app/services/api.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.page.html',
   styleUrls: ['./home.page.scss'],
   standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
+  imports: [IonLabel, IonButton, IonRange, IonSelect, IonList, IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule, IonSelectOption, IonItem]
 })
 export class HomePage implements OnInit {
+  categories: { id: number, name: string }[] = [];
 
-  constructor() { }
+  selectedCategory!: string;
+  selectedDifficulty!: string;
+  selectedAmount: number = 5;
+
+  constructor(private _api: ApiService) { }
 
   ngOnInit() {
+    this.getCategories();
   }
 
+  getCategories() {
+    this._api.getCategories().subscribe({
+      next: (data) => {
+        this.categories = data.trivia_categories;
+      },
+      error: (e) => {
+        console.log(e);
+      }
+    });
+  }
+
+  changeCategory(event: CustomEvent) {
+    this.selectedCategory = event.detail.value;
+  }
+
+  changeDifficulty(event: CustomEvent) {
+    this.selectedDifficulty = event.detail.value;
+  }
+
+  changeAmount(event: CustomEvent) {
+    this.selectedAmount = parseInt(event.detail.value);
+  }
+
+  startGame() {
+    this._api.getQuestions(this.selectedAmount, this.selectedCategory, this.selectedDifficulty).subscribe({
+      next: (data) => {
+        console.log(data);
+      },
+      error: (e) => {
+        console.log(e);
+      },
+    });
+  }
 }
