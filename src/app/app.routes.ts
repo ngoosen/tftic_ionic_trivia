759bfdc327fea29635b4a6354eadf9362209b829
Noémie Goosen <noemie.goosen@gmail.com>
2025-03-11 14:51:54 +0100

    feat: update routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -7,25 +7,26 @@ export const routes: Routes = [
     component: TabsPage,
     children: [
       {
+        path: "",
+        pathMatch: "full",
+        redirectTo: "home",
+      },
+      {
         path: "home",
         loadComponent: () => import('./pages/home/home.page').then((m) => m.HomePage),
-      }
+      },
+      {
+        path: 'quizz',
+        loadComponent: () => import('./pages/quizz/quizz.page').then( m => m.QuizzPage)
+      },
+      {
+        path: 'results',
+        loadComponent: () => import('./pages/results/results.page').then( m => m.ResultsPage)
+      },
+      {
+        path: 'history',
+        loadComponent: () => import('./pages/history/history.page').then( m => m.HistoryPage)
+      },
     ]
-  },
-  {
-    path: 'home',
-    loadComponent: () => import('./pages/home/home.page').then( m => m.HomePage)
-  },
-  {
-    path: 'quizz',
-    loadComponent: () => import('./pages/quizz/quizz.page').then( m => m.QuizzPage)
-  },
-  {
-    path: 'results',
-    loadComponent: () => import('./pages/results/results.page').then( m => m.ResultsPage)
-  },
-  {
-    path: 'history',
-    loadComponent: () => import('./pages/history/history.page').then( m => m.HistoryPage)
   },
 ];
