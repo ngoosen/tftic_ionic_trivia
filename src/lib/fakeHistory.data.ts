1e43788d522067135b4fa867c6bc5b9aa23bd678
Noémie Goosen <noemie.goosen@gmail.com>
2025-03-12 16:34:34 +0100

    feat: add fake data

---
+++ src/lib/fakeHistory.data.ts
@@ -0,0 +1,39 @@
+import { IGame } from "src/app/services/database.service";
+
+export const fakeData: IGame[] = [
+  {
+    category: "9",
+    difficulty: "easy",
+    score: 7,
+    total_questions: 10,
+    date: 1741788199,
+  },
+  {
+    category: "",
+    difficulty: "",
+    score: 2,
+    total_questions: 5,
+    date: 1741119139,
+  },
+  {
+    category: "11",
+    difficulty: "",
+    score: 10,
+    total_questions: 10,
+    date: 1740555679,
+  },
+  {
+    category: "9",
+    difficulty: "medium",
+    score: 12,
+    total_questions: 15,
+    date: 1738501699,
+  },
+  {
+    category: "",
+    difficulty: "hard",
+    score: 0,
+    total_questions: 10,
+    date: 1738509259,
+  }
+];
